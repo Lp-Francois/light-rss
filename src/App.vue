f932f40f3965f0b15faf0328869398658d97708e
François <asteryujano@gmail.com>
2019-04-27 17:19:41 +0200

    dark theme toggle done

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,26 @@
 <template>
 	<div id="app" data-theme="light">
-		<header class="header">
-			<nav class="container grid-lg nav-header">
-				<a href="https://github.com/AsterYujano/light-rss">
-					<img class="logo" alt="logo" src="./assets/vue-rss.png">
-				</a>
-				<h4 class="title">Light-rss</h4>
-			</nav>
-		</header>
+		<div>
+			<Header/>
+		</div>
 
 		<div class="container grid-lg">
 			<Rss/>
 		</div>
-
 	</div>
 </template>
 
 <script>
-
-
-/* View */
-import Rss from './components/Rss'
+import Header from './components/Header.vue';
+import Rss from './components/Rss.vue';
 
 export default {
 	name: 'app',
 	components: {
-		Rss
+		Header,
+		Rss,
 	},
 }
-
 </script>
 
 <style>
@@ -46,12 +38,11 @@ export default {
     --primary-color: #42b883;
     --secondary-color: #818cab;
     --font-color: #e1e1ff;
-    --font-color-visited: #f5f5ff;
+    --font-color-visited: rgb(169, 169, 179);;
     --bg-color: #181E24;
     --secondary-bg-color: #1F2831;
     --heading-color: #818cab;
 }
-
 
 #app {
 	font-family: 'Avenir', Helvetica, Arial, sans-serif;
@@ -62,33 +53,7 @@ export default {
 	min-height: 100vh;
 }
 
-.header {
-	height: 45px;
-	background-color: var(--secondary-bg-color);
-	margin-bottom: 15px;
-	box-shadow: 0 2px 4px 0 rgba(0,0,0,0.13);
-}
-
-.nav-header {
-	display: flex;
-	flex-direction: row;
-	justify-content: flex-start;
-	align-items: center;
-}
-
-.title {
-	margin-bottom: 0;
-}
-
-.logo {
-	width: 30px;
-	height: 30px;
-	margin-right: 15px;
-	margin-top: 7px;
-}
-
 a {
 	color: var(--primary-color);
 }
-
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,143 @@
+<template>
+	<div>
+		<header class="header">
+			<nav class="container grid-lg nav-header">
+				<a href="https://github.com/AsterYujano/light-rss">
+					<img class="logo" alt="logo" src=".././assets/vue-rss.png">
+				</a>
+				<h4 class="title">Light-rss</h4>
+
+				<label class="switch" v-on:click="toggle">
+					<input type="checkbox">
+					<span class="slider round"></span>
+				</label>
+			</nav>
+		</header>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Header',
+	data() {
+		return {
+
+		}
+	},
+	mounted: function() {
+		const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+		const toggleSwitch = document.querySelector('.switch input[type="checkbox"]');
+
+		if (currentTheme) {
+		    document.documentElement.setAttribute('data-theme', currentTheme);
+
+		    if (currentTheme === 'dark') {
+		        toggleSwitch.checked = true;
+		    }
+		}
+	},
+	methods: {
+		toggle: function(e) {
+			if (e.target.checked) {
+		        document.documentElement.setAttribute('data-theme', 'dark');
+		        localStorage.setItem('theme', 'dark');
+		    }
+		    else {
+		        document.documentElement.setAttribute('data-theme', 'light');
+		        localStorage.setItem('theme', 'light');
+		    }
+		}
+	}
+}
+</script>
+
+<style scoped>
+.header {
+	height: 45px;
+	background-color: var(--secondary-bg-color);
+	margin-bottom: 15px;
+	box-shadow: 0 2px 4px 0 rgba(0,0,0,0.13);
+}
+
+.nav-header {
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+	align-items: center;
+}
+
+.title {
+	margin-bottom: 0;
+}
+
+.logo {
+	width: 30px;
+	height: 30px;
+	margin-right: 15px;
+	margin-top: 7px;
+}
+
+
+/* Switch Checkbox*/
+ /* The switch - the box around the slider */
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 60px;
+  height: 34px;
+}
+
+/* Hide default HTML checkbox */
+.switch input {
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+/* The slider */
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ccc;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 26px;
+  width: 26px;
+  left: 4px;
+  bottom: 4px;
+  background-color: white;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+input:checked + .slider {
+  background-color: var(--primary-color);
+}
+
+input:focus + .slider {
+  box-shadow: 0 0 1px var(--primary-color);
+}
+
+input:checked + .slider:before {
+  -webkit-transform: translateX(26px);
+  -ms-transform: translateX(26px);
+  transform: translateX(26px);
+}
+
+.slider.round {
+  border-radius: 34px;
+}
+
+.slider.round:before {
+  border-radius: 50%;
+} 
+</style>
